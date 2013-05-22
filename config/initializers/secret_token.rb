d42e3844f240f8e68eb5a547ddeb570018d0696d
amrit <amrit@amrit-Inspiron-1545.(none)>
2013-05-22 10:55:29 +0545

    msg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intern::Application.config.secret_token = '04ed7a426f90589cbc6df6f5e5c79830137d4a0085805b9f13aef02fbffcb3dfe8a6baba3528faf91b319d1bd19f4461cf28d3ab6c9d917abd54667bde053b84'
